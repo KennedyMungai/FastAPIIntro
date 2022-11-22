a7f5adcb84ce040da1b1b50e38135be3693894d7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-22 09:43:42 +0300

    Added the update code

--- myapi.py
+++ myapi.py
@@ -60,5 +60,12 @@ def update_student(student_id: int, student: UpdateStudent):
     if student_id not in students:
         return {"Error": "Student does not exist"}
     
+    if student.name != None:
+        students[student_id].name = student.name
+    if student.age != None:
+        students[student_id].age = student.age
+    if student.year != None:
+        students[student_id].year = student.year
+    
     students[student_id] = student
     return students[student_id]
