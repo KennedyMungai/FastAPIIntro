cdd8886a152c7d7b672f5994f585f42960843edb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-22 07:46:16 +0300

    Created a new endpoint that uses a path parameter to fetch data

--- myapi.py
+++ myapi.py
@@ -15,3 +15,8 @@ students = {
 @app.get("/")
 def index():
     return {"name":"First Data"}
+
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int):
+    return students[student_id]
