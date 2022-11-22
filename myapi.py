c6ea7bfa8ff003aeb7acf628cf686d6e56eb98b7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-22 07:42:43 +0300

    Created the students dictionary

--- myapi.py
+++ myapi.py
@@ -3,6 +3,15 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+students = {
+    1: {
+        "name" : "john",
+        "age" : 17,
+        "class": "year 12"
+    }
+}
+
+
 @app.get("/")
 def index():
     return {"name":"First Data"}
