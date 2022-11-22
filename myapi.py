4d307c66c98868960a61dcfbb7d1f6faeafb5bcd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-22 09:39:14 +0300

    Returned the students data structure item without updating the items

--- myapi.py
+++ myapi.py
@@ -59,3 +59,6 @@ def create_student(student_id: int, student: Student):
 def update_student(student_id: int, student: UpdateStudent):
     if student_id not in students:
         return {"Error": "Student does not exist"}
+    
+    students[student_id] = student
+    return students[student_id]
